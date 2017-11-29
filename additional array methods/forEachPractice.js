c1582a4d5ca3ec2fe060741c6acbe8d422413fef
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2017-11-29 11:25:31 -0700

    recent update

--- additional array methods/forEachPractice.js
+++ additional array methods/forEachPractice.js
@@ -2,6 +2,13 @@ var numbers = [3, 6, 9, 12, 15];
 
 //forEach over numbers and add 1 to each number
 //update the original array
+numbers.forEach(function (number,index,array){
+    numbers;
+    var nexNum = number + 1;
+    numbers[index]= nexNum;
+})
+numbers;
+
 
 var employees = [
     { name: "aaron", pay: 12 },
@@ -11,3 +18,9 @@ var employees = [
 ]
 
 //forEach over employees and give each employee a bump in pay by 3
+employees.forEach(function(employee,index,array){
+    employee.pay = employee.pay + 3;
+})
+
+
+employees;
